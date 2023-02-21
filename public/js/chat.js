5cb20e542da63587f9e375adda666c9b7fdd18c7
Diogo <dinash@outlook.com>
2023-02-21 15:18:34 -0300

    first commit

---
+++ public/js/chat.js
@@ -0,0 +1,178 @@
+const socket = io();
+const inputQuestion = document.getElementById("inputQuestion");
+const result = document.getElementById("result");
+
+inputQuestion.addEventListener("keypress", (e) => {
+  if (inputQuestion.value && e.key === 'Enter')
+  SendQuestion();
+});
+
+const OPENAI_API_KEY = "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";
+
+function SendQuestion() {
+  var sQuestion = inputQuestion.value;
+  
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: sQuestion,
+      max_tokens: 2048,
+      temperature: 0.5,
+      //stop["\n"]
+      //top_p=1,
+      //frequency_penalty=0,
+      //presence_penalty=0,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (result.value) result.value += "\n";
+
+      if (json.error?.message) {
+        result.value += `Error: ${json.error.message}`;
+      } else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Sem resposta";
+
+        result.value += 'Chat GPT: ' + text;
+      }
+
+      result.scrollTop = result.scrollHeight;
+    })
+    .catch((error) => console.error("Error:", error))
+    .finally(() => {
+      inputQuestion.value = "";
+      inputQuestion.disabled = false;
+      inputQuestion.focus();
+    });
+
+  if (result.value) result.value += "\n\n\n";
+
+  result.value += `Eu: ${sQuestion}`;
+  inputQuestion.value = "Carregando..";
+  inputQuestion.disabled = true;
+
+  result.scrollTop = result.scrollHeight;
+}
+
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $messages = document.querySelector('#messages');
+const $locationMessage = document.querySelector('#messages');
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#locationMessage-template').innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
+
+//Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+const autoscroll = () => {
+  //New message element
+  const $newMessage = $messages.lastElementChild
+
+  // Height of the last message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  //Visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  //Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if(containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+}
+
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a')
+  })
+  $messages.insertAdjacentHTML('beforeend', html);
+  autoscroll();
+});
+
+socket.on('locationMessage', (locationMessage) => {
+  console.log(locationMessage);
+  const html = Mustache.render(locationMessageTemplate, {
+    username: locationMessage.username,
+    message: locationMessage.text,
+    createdAt: moment(locationMessage.createdAt).format('h:mm a')
+  })
+  $locationMessage.insertAdjacentHTML('beforeend', html);
+  autoscroll();
+})
+
+socket.on('roomData', ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users
+  })
+  document.querySelector('#sidebar').innerHTML = html;
+})
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute('disabled', 'disabled');
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+    
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message delivered");
+  });
+});
+
+const $sendLocation = document.querySelector('#send-location');
+
+$sendLocation.addEventListener("click", () => {
+  
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  $sendLocation.setAttribute('disabled', 'disabled');
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    position = { lat, long };
+
+    socket.emit("sendLocation", position, (callback) => {
+      $sendLocation.removeAttribute('disabled');
+      console.log("Location Shared");
+    });
+  });
+});
+
+socket.emit('join', { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = '/';
+  }
+})
